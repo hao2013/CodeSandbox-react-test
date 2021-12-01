6ecda3038ebaf7965964216d3460ac31e53a8309
hao2013 <67059492+hao2013@users.noreply.github.com>
2021-12-01 23:01:44 +0900

    stateの使い方1 /新しいstateが前のstateに基づいて計算される場合、関数型で書く

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  console.log("APP");
   const [count, setCount] = useState(10000);
+  const onClickCount = () => {
+    //新しいstateが前のstateに基づいて計算される場合、関数型で書く
+    setCount(() => j + 1);
+    setCount((j) => j + 1);
+    setCount((i) => i + 1);
+  };
 
   return (
     <div className="App">
       <h2>hello!!</h2>
       <p>{count}</p>
+      <button onClick={onClickCount}>クリックしてね</button>
     </div>
   );
 }
