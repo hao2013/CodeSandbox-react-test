95ff8ae7319273bf520d54cd79d7c4d7c9fcd842
hao2013 <67059492+hao2013@users.noreply.github.com>
2021-12-01 23:03:46 +0900

    useStateの使い方1 新しいstateが前のstateに基づいて計算される場合、関数型で書く

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   const [count, setCount] = useState(10000);
   const onClickCount = () => {
     //新しいstateが前のstateに基づいて計算される場合、関数型で書く
-    setCount(() => j + 1);
+    setCount((prevCount) => prevCount + 1);
     setCount((j) => j + 1);
     setCount((i) => i + 1);
   };
